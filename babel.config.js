98b279d6aba37206f34bf46f6373404711fbe882
Jayson Martinez <jlmartinez631@gmail.com>
2022-08-21 15:19:10 +0800

    Implement module alias

--- babel.config.js
+++ babel.config.js
@@ -13,5 +13,28 @@ module.exports = {
         runtime: 'automatic',
       },
     ],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '^~/(.+)': './src/\\1',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.ts',
+          '.android.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
   ],
 };
