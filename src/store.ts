f5d80aecc48c1ea91b6adc8eb440ebdbe982895d
Jayson Martinez <jlmartinez631@gmail.com>
2022-10-08 21:06:21 +0800

    Initial redux setup

---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+/* Types */
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
